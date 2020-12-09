34cc2f518cafa774879b6f2cc0fd9386f6c9849b
Alexandre Wavelet <wavelet.alexandre@gmail.com>
2020-12-09 22:04:26 +0100

    P2

--- app/Days/Eight.php
+++ app/Days/Eight.php
@@ -3,6 +3,7 @@
 namespace App\Days;
 
 use Exception;
+use Illuminate\Support\Collection;
 
 class Eight extends Day
 {
@@ -25,21 +26,23 @@ HTML;
         $accumulator = $this->getAccumulatorValueBeforeLoop();
 
         return <<<HTML
-        <p>The accumulator value before the bootloop is <b>$accumulator</b></p>
+        <p>The accumulator value before the bootloop is <b>$accumulator</b>.</p>
 HTML;
     }
 
     public function secondPuzzle(): string
     {
+        $accumulator = $this->fixBoot();
+
         return <<<HTML
-        <p>Ahah<p>
+        <p>When fixing the boot, we get an accumulator of <b>$accumulator</b>.<p>
 HTML;
     }
 
     public function getAccumulatorValueBeforeLoop(): int
     {
         $accumulator = 0;
-        $dataset = $this->dataset;
+        $dataset = clone $this->dataset;
         $instruction_key = 0;
         $instruction_matches = [];
 
@@ -75,6 +78,82 @@ HTML;
             );
             [, $instruction, $number] = $instruction_matches;
             $number = intval($number);
+        }
+
+        return $accumulator;
+    }
+
+    public function fixBoot(): int
+    {
+        $accumulator = 0;
+        $dataset = $this->dataset->toArray();
+
+        while(($row = next($dataset)) && !$accumulator) {
+            if (strpos($row, 'acc') === 0) {
+                continue;
+            }
+
+            try {
+                $copy = $dataset;
+                $copy[key($dataset)] = str_replace(
+                    [self::NOP, self::JMP],
+                    [self::JMP, self::NOP],
+                    $row
+                );
+
+                $accumulator = $this->testBoot(collect($copy));
+            } catch (\Throwable $e) {}
+        }
+
+        return $accumulator;
+    }
+
+    public function testBoot(Collection $dataset): int
+    {
+        $accumulator = 0;
+        $instruction_key = 0;
+        $instruction_matches = [];
+
+        preg_match("/(nop|acc|jmp|stop) ([\+-]\d+)/", $dataset->first(), $instruction_matches);
+        [, $instruction, $number] = $instruction_matches;
+        $number = intval($number);
+
+        while ($instruction) {
+            $dataset->put($instruction_key, self::STOP . ' +0');
+
+            switch ($instruction) {
+                case Self::ACC:
+                    $accumulator += $number;
+                    $instruction_key++;
+                    break;
+
+                case self::JMP:
+                    $instruction_key += $number;
+                    break;
+
+                case self::NOP:
+                    $instruction_key++;
+                    break;
+
+                case self::STOP:
+                    throw new Exception('Bootloop!');
+
+                default:
+                    throw new Exception('Hey you! Watcha doing here?');
+            }
+
+            preg_match(
+                "/(nop|acc|jmp|stop) ([\+-]\d+)/",
+                $dataset->get($instruction_key),
+                $instruction_matches
+            );
+
+            if (!$instruction_matches) {
+                $instruction = false;
+            } else {
+                [, $instruction, $number] = $instruction_matches;
+                $number = intval($number);
+            }
         }
 
         return $accumulator;
--- tests/Unit/EightTest.php
+++ tests/Unit/EightTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 class EightTest extends TestCase
 {
     /** @test */
-    public function itDoesSomething(): void
+    public function itFindsAccumulatorValueBeforeLoop(): void
     {
         $day = new Eight(collect([
             'nop +0',
@@ -23,5 +23,23 @@ class EightTest extends TestCase
         ]));
 
         $this->assertEquals(5, $day->getAccumulatorValueBeforeLoop());
+    }
+
+    /** @test */
+    public function itFixesTheProgramAndReturnAccumulator(): void
+    {
+        $day = new Eight(collect([
+            'nop +0',
+            'acc +1',
+            'jmp +4',
+            'acc +3',
+            'jmp -3',
+            'acc -99',
+            'acc +1',
+            'jmp -4',
+            'acc +6',
+        ]));
+
+        $this->assertEquals(8, $day->fixBoot());
     }
 }
